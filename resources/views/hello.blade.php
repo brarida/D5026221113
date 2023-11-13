a739f1d399fac7dd901b625dec4e73cba4f04eaa
brarida <aufaabrarida111@gmail.com>
2023-11-13 13:27:13 +0700

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Aufaa Hafizhah Brarida : 5026221113 </title>
+    <style>
+        a:visited{
+            color: black;
+            text-decoration: none;
+        }
+        a:link{
+            color: cadetblue;
+            text-decoration: none;
+        }
+        a:hover{
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body bgcolor="#ced0f2">
+    <p style="color:red">Jenis Jenis Penulisan</p>
+    <h1 style="background-color:white;">Selamat Datang di IndoMaret</h1>
+    <h2 style="color:white">Hallo, Apa kabar?</h2>
+    Hallo, Apa kabar?<br>
+    <p>Hallo, Apa kabar? baik, kalau anda?</p>
+
+    <p style="color:red">Macam-Macam Hyperlink</p>
+    <a href="https://my.its.ac.id/">web ITS</a>
+    |
+    <a href="https://www.detik.com/" target="_blank">detik.com</a>
+    |
+    <a href="https://www.w3schools.com/" target="_parent">w3schools HTML</a>
+
+    <p style="color:red">Macam-Macam Export Media</p>
+    <br>
+    <img src="download.jpeg" class="fotolingkaran">
+</body>
+</html>
